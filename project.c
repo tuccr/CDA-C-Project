6062ebdad1cef92d1ae3485bfe3a8eacfd321940
tuccr <metuccr@gmail.com>
2023-11-15 18:20:50 -0500

    notes

--- project.c
+++ project.c
@@ -1,5 +1,8 @@
 #include "spimcore.h"
 
+// BINARY CAN BE DONE WITH 0b001 AND HEX CAN BE DONE WITH 0x001
+
+
 
 /* ALU */
 /* 10 Points */
@@ -21,7 +24,7 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
             else{*ALUresult = 0; *Zero = 1;}
             break;
         case 3: //slt unsigned; 011
-           if((A << 1) < (B << 1)) {*ALUresult = 1; *Zero = 0;}
+           if(A < B) {*ALUresult = 1; *Zero = 0;}
            else {*ALUresult = 0; *Zero = 1;}
            break; 
         case 4: //A AND B; 100
@@ -50,7 +53,9 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
-    
+    // check is PC is divisible by 4 then divide by 4 (PC >> 4) to tget the actual location (Mem is an array of words)
+    // spimcore.c -> #define MEM(addr) MEM[(addr >> 4)] should do this
+    // can halt
 }
 
 
@@ -62,16 +67,16 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1, uns
 
     //based on Step() contents in spimcore.c this needs to take instruction and simply copy parts into each thing (tear it to pieces)
 
-    int n = 0; //placeholder for sake of example, change to actual number
+    int n = 0;
 
     // NOTE: instead of shifting, just get binary number equal to all 1s in the respective position
-    *op = (instruction & n);
-    *r1 = (instruction & n);
-    *r2 = (instruction & n);
-    *r3 = (instruction & n);
-    *offset = (instruction & n);
-    *funct = (instruction & 63); // 63 is 111111
-    *jsec = (instruction & 33554431); // this number is 26 ones
+    *op = (instruction & 0b11111100000000000000000000000000) >> 26;
+    *r1 = (instruction & 0b00000011111000000000000000000000) >> 21;
+    *r2 = (instruction & 0b00000000000111110000000000000000) >> 16;
+    *r3 = (instruction & 0b00000000000000001111100000000000) >> 11;
+    *offset = (instruction & 0b000000000000000000000011111000000) >> 6;
+    *funct = (instruction & 0b00000000000000000000000000111111) >> 0;
+    *jsec = (instruction & 0b00000011111111111111111111111111) >> 0;
 }
 
 
@@ -84,10 +89,10 @@ int instruction_decode(unsigned op,struct_controls *controls)
     // 0 or 1, 2 means don't care
     // return 1 if halt, 0 otherwise
 
-    // currently hardcoded, maybe find way to make it not hard coded
+    // currently hardcoded (table route), make sure all values are correct 
 
     switch(op) {
-        case 0: //R-type instruction, operations will be determined by funct so return nothing
+        case 0: //R-type instruction, 000000
             controls->RegDst = 2;
             controls->Jump = 2;
             controls->Branch = 2;
@@ -98,7 +103,7 @@ int instruction_decode(unsigned op,struct_controls *controls)
             controls->ALUSrc = 2;
             controls->RegWrite = 2;
             return 0;
-        case 2: //jump, 000010
+        case 2: //jump, 0b000010
             controls->RegDst = 0;
             controls->Jump = 0;
             controls->Branch = 0;
@@ -109,7 +114,7 @@ int instruction_decode(unsigned op,struct_controls *controls)
             controls->ALUSrc = 0;
             controls->RegWrite = 0;
             return 0;
-        case 4: //branch eq, 000100
+        case 4: //branch eq, 0b000100
             controls->RegDst = 0;
             controls->Jump = 0;
             controls->Branch = 0;
@@ -120,7 +125,7 @@ int instruction_decode(unsigned op,struct_controls *controls)
             controls->ALUSrc = 0;
             controls->RegWrite = 0;
             return 0;
-        case 8: //add immediate, 001000
+        case 8: //add immediate, 0b001000
             controls->RegDst = 0;
             controls->Jump = 0;
             controls->Branch = 0;
@@ -131,7 +136,7 @@ int instruction_decode(unsigned op,struct_controls *controls)
             controls->ALUSrc = 0;
             controls->RegWrite = 0;
             return 0;
-        case 10: //set on less than immediate, 001010
+        case 10: //set on less than immediate, 0b001010
             controls->RegDst = 0;
             controls->Jump = 0;
             controls->Branch = 0;
@@ -142,7 +147,7 @@ int instruction_decode(unsigned op,struct_controls *controls)
             controls->ALUSrc = 0;
             controls->RegWrite = 0;
             return 0;
-        case 11: //set on less than immediate unsigned, 001011
+        case 11: //set on less than immediate unsigned, 0b001011
             controls->RegDst = 0;
             controls->Jump = 0;
             controls->Branch = 0;
@@ -153,7 +158,7 @@ int instruction_decode(unsigned op,struct_controls *controls)
             controls->ALUSrc = 0;
             controls->RegWrite = 0;
             return 0;
-        case 12: //and immediate, 001100
+        case 12: //and immediate, 0b001100
             controls->RegDst = 0;
             controls->Jump = 0;
             controls->Branch = 0;
@@ -164,7 +169,7 @@ int instruction_decode(unsigned op,struct_controls *controls)
             controls->ALUSrc = 0;
             controls->RegWrite = 0;
             return 0;
-        case 13: //or immediate, 001101
+        case 13: //or immediate, 0b001101; may not be needed as instructions say only OR literal with opcode 0b000000
             controls->RegDst = 0;
             controls->Jump = 0;
             controls->Branch = 0;
@@ -175,7 +180,7 @@ int instruction_decode(unsigned op,struct_controls *controls)
             controls->ALUSrc = 0;
             controls->RegWrite = 0;
             return 0;
-        case 15: //load upper immediate, 001111
+        case 15: //load upper immediate, 0b001111
             controls->RegDst = 0;
             controls->Jump = 0;
             controls->Branch = 0;
@@ -186,18 +191,18 @@ int instruction_decode(unsigned op,struct_controls *controls)
             controls->ALUSrc = 0;
             controls->RegWrite = 0;
             return 0;
-        case 131: //load word, 100011
+        case 131: //load word, 0b100011
             controls->RegDst = 0;
             controls->Jump = 0;
             controls->Branch = 0;
             controls->MemRead = 0;
             controls->MemtoReg = 0;
-            controls->ALUOp = 0;
+            controls->ALUOp = 0b000;
             controls->MemWrite = 0;
             controls->ALUSrc = 0;
             controls->RegWrite = 0;
             return 0;
-        case 163: //store word, 101011
+        case 163: //store word, 0b101011
             controls->RegDst = 0;
             controls->Jump = 0;
             controls->Branch = 0;
@@ -219,7 +224,7 @@ int instruction_decode(unsigned op,struct_controls *controls)
 /* 5 Points */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
-
+    // do not need to be word aligned, doesn't return a halt
 }
 
 
@@ -227,21 +232,26 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
-    // unsigned long long int is 8 bytes, not sure if needed
+    // unsigned long long int is 8 bytes, not sure we need to use long long int but definitely need to keep sign 
+    // offset is input and extended_value is output
 }
 
 /* ALU operations */
 /* 10 Points */
 int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
 {
-
+    // look at ALUOp and determine if R-type or not
+        // if yes, decode funct
+    // call ALU() and determine if halt
 }
 
 /* Read / Write Memory */
 /* 10 Points */
 int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
 {
-
+    // if MemWrite = 1, check word alignment and write into memory
+    // if MemRead = 1, check word alignment and read from memory
+    // IF NOT WORD ALIGNED, RETURN HALT
 }
 
 
@@ -249,7 +259,7 @@ int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsig
 /* 10 Points */
 void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
 {
-
+    // look at control signals and write as necessary
 }
 
 /* PC update */
