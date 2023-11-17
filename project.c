e617810c97d8b0fb786ac9ad0834a93820955831
tuccr <metuccr@gmail.com>
2023-11-17 12:52:44 -0500

    sutff

--- project.c
+++ project.c
@@ -9,39 +9,40 @@
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 {
     unsigned invA = ~A + 1;
+    printf("A = %d\ninvA = %d\n", A, invA);
     unsigned invB = ~B + 1;
     //~X + 1 will work no matter if X is actually positive or negative
 
     switch(ALUControl) {
-        case 0: //add; 000
-           *ALUresult = A + B;
-           break;
-        case 1: //subtract; 001
-           *ALUresult = A + invB;
-           break;
-        case 2: //if A < B, Z = 1; otherwise, Z = 0; 010
+        case 0b000: //add; 000
+            *ALUresult = A + B;
+            break;
+        case 0b001: //subtract; 001
+            *ALUresult = A + invB;
+            break;
+        case 0b010: //if A < B, Z = 1; otherwise, Z = 0; 010
             if(A + invB < 0) {*ALUresult = 1; *Zero = 0;}
             else{*ALUresult = 0; *Zero = 1;}
             break;
-        case 3: //slt unsigned; 011
-           if(A < B) {*ALUresult = 1; *Zero = 0;}
-           else {*ALUresult = 0; *Zero = 1;}
-           break; 
-        case 4: //A AND B; 100
-           *ALUresult = (A & B);
-           break;
-        case 5: //A OR B; 101
-           *ALUresult = (A | B);
-           break;
-        case 6: //shift B left by 16 bits; 110
-           *ALUresult = (B << 16);
-           break;
-        case 7: //NOT A; 111
-           *ALUresult = ~A;
-           break;
+        case 0b011: //slt unsigned; 011
+            if(A < B) {*ALUresult = 1; *Zero = 0;}
+            else {*ALUresult = 0; *Zero = 1;}
+            break;
+        case 0b100: //A AND B; 100
+            *ALUresult = (A & B);
+            break;
+        case 0b101: //A OR B; 101
+            *ALUresult = (A | B);
+            break;
+        case 0b110: //shift B left by 16 bits; 110
+            *ALUresult = (B << 16);
+            break;
+        case 0b111: //NOT A; 111
+            *ALUresult = ~A;
+            break;
         default:
-           *ALUresult = 0;
-           break;
+            *ALUresult = 0;
+            break;
     }
 }
 
@@ -53,9 +54,17 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
-    // check is PC is divisible by 4 then divide by 4 (PC >> 4) to tget the actual location (Mem is an array of words)
-    // spimcore.c -> #define MEM(addr) MEM[(addr >> 4)] should do this
+    // check is PC is divisible by 4 then divide by 4 (PC >> 2) to get the actual location (Mem is an array of words)
+    // spimcore.c -> #define MEM(addr) MEM[(addr >> 2)] should do this
     // can halt
+
+    if(PC % 4 != 0) {
+        return 1;
+    }
+    else {
+        *instruction = Mem[PC >> 2]; // if this doesn't work try MEM(PC);
+        return 0;
+    }
 }
 
 
@@ -65,11 +74,10 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1, uns
 {
     // see projectDetails.pptx
 
-    //based on Step() contents in spimcore.c this needs to take instruction and simply copy parts into each thing (tear it to pieces)
-
-    int n = 0;
+    // based on Step() contents in spimcore.c this needs to take instruction and simply copy parts into each thing (tear it to pieces)
 
     // NOTE: instead of shifting, just get binary number equal to all 1s in the respective position
+
     *op = (instruction & 0b11111100000000000000000000000000) >> 26;
     *r1 = (instruction & 0b00000011111000000000000000000000) >> 21;
     *r2 = (instruction & 0b00000000000111110000000000000000) >> 16;
@@ -98,7 +106,7 @@ int instruction_decode(unsigned op,struct_controls *controls)
             controls->Branch = 2;
             controls->MemRead = 2;
             controls->MemtoReg = 2;
-            controls->ALUOp = 2;
+            controls->ALUOp = 0;
             controls->MemWrite = 2;
             controls->ALUSrc = 2;
             controls->RegWrite = 2;
@@ -158,28 +166,6 @@ int instruction_decode(unsigned op,struct_controls *controls)
             controls->ALUSrc = 0;
             controls->RegWrite = 0;
             return 0;
-        case 12: //and immediate, 0b001100
-            controls->RegDst = 0;
-            controls->Jump = 0;
-            controls->Branch = 0;
-            controls->MemRead = 0;
-            controls->MemtoReg = 0;
-            controls->ALUOp = 0;
-            controls->MemWrite = 0;
-            controls->ALUSrc = 0;
-            controls->RegWrite = 0;
-            return 0;
-        case 13: //or immediate, 0b001101; may not be needed as instructions say only OR literal with opcode 0b000000
-            controls->RegDst = 0;
-            controls->Jump = 0;
-            controls->Branch = 0;
-            controls->MemRead = 0;
-            controls->MemtoReg = 0;
-            controls->ALUOp = 0;
-            controls->MemWrite = 0;
-            controls->ALUSrc = 0;
-            controls->RegWrite = 0;
-            return 0;
         case 15: //load upper immediate, 0b001111
             controls->RegDst = 0;
             controls->Jump = 0;
@@ -224,7 +210,9 @@ int instruction_decode(unsigned op,struct_controls *controls)
 /* 5 Points */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
-    // do not need to be word aligned, doesn't return a halt
+    // does not need to be word aligned, doesn't return a halt
+    *data1 = Reg[r1 >> 4];
+    *data2 = Reg[r2 >> 4];
 }
 
 
@@ -232,8 +220,14 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
-    // unsigned long long int is 8 bytes, not sure we need to use long long int but definitely need to keep sign 
     // offset is input and extended_value is output
+
+    if(((1 << 15) & offset) == (1 << 15)) {
+        *extended_value = offset + 0b11111111111111110000000000000000;
+    }
+    else {
+        *extended_value = offset;
+    }
 }
 
 /* ALU operations */
@@ -243,6 +237,38 @@ int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigne
     // look at ALUOp and determine if R-type or not
         // if yes, decode funct
     // call ALU() and determine if halt
+
+    if(ALUOp == 2 && ALUSrc == 2) {
+        if(extended_value > (65536 >> 2)) {
+            return 1;
+        }
+    }
+
+    if(ALUSrc == 1) {
+        data2 = extended_value;
+        switch(funct) {
+            case 0b100000:
+                ALUOp = 0b000;//add
+                break;
+            case 0b100010:
+                ALUOp = 0b001;//subtract
+                break;
+            case 0b100100:
+                ALUOp = 0b100;//and
+                break;
+            case 0b100101:
+                ALUOp = 0b101;//or
+                break;
+            case 0b100111:
+                ALUOp = 0b111;//not
+                break;
+            default:
+                return 1;
+        }
+    }
+
+    ALU(data1, data2, ALUOp, ALUresult, Zero);
+    return 0;
 }
 
 /* Read / Write Memory */
